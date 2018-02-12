8ea920c90544e00a1fb8a5757fe4c24e938a46b6
Ken Rayner <kenneth.rayner@digital.hmrc.gov.uk>
2018-02-12 17:10:16 +0000

    save addNum Kata

---
+++ src/main/scala/numAnimals.scala
@@ -0,0 +1,24 @@
+
+
+  object numAnimals extends App {
+
+    def howMany(heads:Int, legs:Int):(Any) = {
+
+      if ((legs % 2 == 0)&&(heads< legs/2)) {
+
+
+        val numChicks = (((4 * heads) - legs) / 2)
+        val numCows = ((legs - (2 * heads)) / 2)
+
+        (numChicks, numCows)
+      }
+      else {
+        ("No solution")
+      }
+    }
+      println (howMany(5,8))
+
+  }
+
+
+
