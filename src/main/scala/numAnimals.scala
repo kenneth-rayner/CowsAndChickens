e0b48945857f2b7042e40826f7ed4ae7eb403a15
Ken Rayner <kenneth.rayner@digital.hmrc.gov.uk>
2018-02-12 18:01:49 +0000

    refactored anilam legs code

--- src/main/scala/numAnimals.scala
+++ src/main/scala/numAnimals.scala
@@ -1,24 +1,21 @@
-
-
+import scala.io.StdIn
 object numAnimals extends App {
 
-  def howMany(heads:Int, legs:Int):(Any) = {
-
-    if ((legs % 2 == 0)&&(heads< legs/2)) {
-
-
+  def howMany(heads: Int, legs: Int): (Any) = {
+    if (legs % 2 == 0) {
       val numChicks = (((4 * heads) - legs) / 2)
       val numCows = ((legs - (2 * heads)) / 2)
+      if ((numChicks >= 0) && (numCows >= 0)) {
 
-      (numChicks, numCows)
-    }
-    else {
-      ("No solution")
+        (numChicks, numCows)
+      }
+      else {
+        ("No solution found")
+      }
     }
   }
-  println (howMany(5,8))
+  val numHeads = StdIn.readLine("Enter number of heads:").toInt
+  val numLegs = StdIn.readLine("Enter number of legss:").toInt
+ println(howMany(numHeads,numLegs))
 
 }
-
-
-
