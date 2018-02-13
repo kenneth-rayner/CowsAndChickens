3c074db7636c691caaa1bc6056dfa1fe1f416323
Ken Rayner <kenneth.rayner@digital.hmrc.gov.uk>
2018-02-13 08:47:06 +0000

    refactor code

--- src/main/scala/numAnimals.scala
+++ src/main/scala/numAnimals.scala
@@ -3,23 +3,20 @@ object numAnimals extends App {
 
   def howMany(heads: Int, legs: Int): (Any) = {
     if (legs % 2 == 0) {
-      val numChicks = (((4 * heads) - legs) / 2)
-      val numCows = ((legs - (2 * heads)) / 2)
+      val numChicks = ((4 * heads) - legs) / 2
+      val numCows = (legs - (2 * heads)) / 2
+
       if ((numChicks >= 0) && (numCows >= 0)) {
 
-        (numChicks, numCows)
+        (numChicks,numCows)
       }
-      else {
-      ("No solution found")
-      }
-
+      else
+        "No solution found"
 
     }
-   else ("No solution found")
-
+    else "No solution found"
   }
   val numHeads = StdIn.readLine("Enter number of heads:").toInt
   val numLegs = StdIn.readLine("Enter number of legss:").toInt
- println(howMany(numHeads,numLegs))
-
+  println(howMany(numHeads, numLegs))
 }
