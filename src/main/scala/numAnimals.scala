736711d28006202d9f1dc5c786021ea3a18923d1
kenneth-rayner <kenneth.rayner@digital.hmrc.gov.uk>
2018-05-02 18:44:29 +0100

    amend code for inout from user

--- src/main/scala/numAnimals.scala
+++ src/main/scala/numAnimals.scala
@@ -1,15 +1,18 @@
 import scala.io.StdIn
+import scala.language.postfixOps
 
 object numAnimals extends App {
 
   def howMany(heads: Int, legs: Int): (Any) = {
+
     if (legs % 2 == 0) {
       val numChicks = ((4 * heads) - legs) / 2
       val numCows = (legs - (2 * heads)) / 2
 
       if ((numChicks >= 0) && (numCows >= 0)) {
-
+        println(s"Number of chickens:$numChicks\nNumber of cow:$numCows" )
         (numChicks,numCows)
+
       }
       else
         "No solution found"
@@ -17,7 +20,8 @@ object numAnimals extends App {
     }
     else "No solution found"
   }
+
   val numHeads = StdIn.readLine("Enter number of heads:").toInt
-  val numLegs = StdIn.readLine("Enter number of legss:").toInt
+  val numLegs = StdIn.readLine("Enter number of legs:").toInt
   println(howMany(numHeads, numLegs))
 }
